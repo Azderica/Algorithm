6547ad635f14495f3f9a5bc04c88d3f0764e9a42
Myunghun Park <42582516+Azderica@users.noreply.github.com>
2020-02-11 23:33:04 +0900

    2020.02.10 baek_10250(Incomplete)

---
+++ baekjoon/10250.cpp
@@ -0,0 +1,17 @@
+#include	<iostream>
+using namespace std;
+
+int main(void) {
+	int T = 0;
+
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		int H = 0, W = 0, N = 0;
+		int x, y, ans;
+		scanf("%d %d %d", &H, &W, &N);
+
+
+	}
+
+	return 0;
+}
