5d45717e4900661c131a02a7c91096c5411c0380
Myunghun Park <mh97888@gmail.com>
2020-02-12 16:06:35 +0900

    Algorithm 20.02.12 pb10250 ver2

--- baekjoon/10250.cpp
+++ baekjoon/10250.cpp
@@ -3,14 +3,23 @@ using namespace std;
 
 int main(void) {
 	int T = 0;
+	int H = 0, W = 0, N = 0;
+	int x, y;
 
 	cin >> T;
 	for (int i = 1; i <= T; i++) {
-		int H = 0, W = 0, N = 0;
-		int x, y, ans;
 		scanf("%d %d %d", &H, &W, &N);
 
+		y = N % H;
+		if (y == 0) 
+			y = H;
+		y *= 100;
 
+		x = N / H + 1;
+		if (!(N % H))
+			x -= 1;
+
+		cout << y + x << endl;
 	}
 
 	return 0;
